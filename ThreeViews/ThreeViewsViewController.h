01bf626a09b037c33301da58330402d25dfd71bc
wannabewize_air <wannabewize_air@Wannabewize-ui-MacBook-Air.local>
2011-03-16 22:33:07 +0900

    Initial Commit

---
+++ ThreeViews/ThreeViewsViewController.h
@@ -0,0 +1,15 @@
+//
+//  ThreeViewsViewController.h
+//  ThreeViews
+//
+//  Created by wannabewize_air on 3/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThreeViewsViewController : UIViewController {
+    
+}
+
+@end
---
+++ ThreeViews/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreeViews
+//
+//  Created by wannabewize_air on 3/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
