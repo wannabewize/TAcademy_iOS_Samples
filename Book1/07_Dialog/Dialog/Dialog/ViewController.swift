c9232d3d8d8c8f38f6af2c30bbb1c15596f1ef33
wannabewize <wannabewize@gmail.com>
2015-10-15 00:02:51 +0900

    Xcode7, iOS9 적용

--- Book1/07_Dialog/Dialog/Dialog/ViewController.swift
+++ Book1/07_Dialog/Dialog/Dialog/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
     let dialog = UIAlertController(title: "계속 진행하겠습니까?", message: "이 곳에 메세지 입력", preferredStyle: UIAlertControllerStyle.Alert)
     
     let cancelAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.Cancel) { (action : UIAlertAction!) -> Void in
-      println("취소 선택")
+      print("취소 선택")
     }
     
     let okAction = UIAlertAction(title: "확인", style: UIAlertActionStyle.Default) { (action) -> Void in
-      println("확인 선택")
+      print("확인 선택")
     }
     
     dialog.addAction(cancelAction)
@@ -32,20 +32,21 @@ class ViewController: UIViewController {
 
     
     let okAction = UIAlertAction(title: "확인", style: UIAlertActionStyle.Destructive) { (action) -> Void in
-      let userInput = dialog.textFields![0] as! UITextField
-      println("사용자 입력 \(userInput.text)")
+      let userInput = dialog.textFields![0]
+      print("사용자 입력 \(userInput.text)")
     }
     dialog.addAction(okAction)
 
     
     let cancelAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.Cancel) { (action) -> Void in
-      println("취소 선택")
+      print("취소 선택")
     }
     dialog.addAction(cancelAction)
     
     
     dialog.addTextFieldWithConfigurationHandler { (textField : UITextField!) -> Void in
-      
+      //textField.keyboardType = UIKeyboardType.NumberPad
+//      textField.secureTextEntry = true
     }
     
     self.presentViewController(dialog, animated: true, completion: nil)
@@ -54,18 +55,18 @@ class ViewController: UIViewController {
     let dialog = UIAlertController(title: "액션시트", message: "메세지", preferredStyle: UIAlertControllerStyle.ActionSheet)
     
     let deleteAction = UIAlertAction(title: "삭제", style: UIAlertActionStyle.Destructive) { (action) -> Void in
-      println("삭제 선택")
+      print("삭제 선택")
     }
     dialog.addAction(deleteAction)
     
     
     let otherAction = UIAlertAction(title: "다른 선택", style: UIAlertActionStyle.Default) { (actino) -> Void in
-      println("다른 선택!")
+      print("다른 선택!")
     }
     dialog.addAction(otherAction)
     
     let cancelAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.Cancel) { (action) -> Void in
-      println("취소 선택")
+      print("취소 선택")
     }
     dialog.addAction(cancelAction)
     
