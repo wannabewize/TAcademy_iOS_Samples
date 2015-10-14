e2044a8d20ddd7c623d1157937de7a57d511a725
Lee Jaehoon <wannabewize@gmail.com>
2015-10-14 20:47:53 +0900

    스택뷰 샘플

---
+++ Book1/04_Layout/StackView/StackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by wannabewize on 2015. 10. 14..
+//  Copyright © 2015년 VanillaStep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
