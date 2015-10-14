3f6cf0b5f6947edf757b884b4686363dfa358609
wannabewize <wannabewize@gmail.com>
2015-10-15 00:07:49 +0900

    Xcode7 적용

--- Book1/08_Picker/DatePicker/DatePicker/ViewController.swift
+++ Book1/08_Picker/DatePicker/DatePicker/ViewController.swift
@@ -9,48 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    // 데이트 피커 객체 생성
-    let datePicker = UIDatePicker()
-    // 위치
-    datePicker.center = self.view.center
-    // 날짜/시간 선택 모드
-    datePicker.datePickerMode = UIDatePickerMode.DateAndTime
-    
-    // 일주일 전 ~ 일주일 후
-    let week : NSTimeInterval = 60 * 60 * 24 * 7
-    datePicker.minimumDate = NSDate(timeIntervalSinceNow: -week)
-    datePicker.maximumDate = NSDate(timeIntervalSinceNow: week)
-    
-    // 뷰 구조에 추가
-    self.view.addSubview(datePicker)
-    
-    // 날짜 변경 이벤트
-    datePicker.addTarget(self, action: Selector("timeChanged:"), forControlEvents: UIControlEvents.ValueChanged)
-  }
-  
-func timeChanged(sender : AnyObject) {
-  let datePicker = sender as! UIDatePicker
-  let date = datePicker.date
-  
-  // 데이트 포매터
-  let dateFormatter = NSDateFormatter()
-  dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
-  dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
-  
-  // 날짜 형식에 맞는 문자열 얻기
-  let dateStr = dateFormatter.stringFromDate(date)
-  println("선택한 날짜 : \(dateStr)")
-}
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // 코드로 추가하기
+      
+      // 데이트 피커 객체 생성
+      let datePicker = UIDatePicker()
+      // 위치
+      datePicker.center.x = self.view.center.x
+      datePicker.center.y = datePicker.frame.size.height;
+      // 날짜/시간 선택 모드
+      datePicker.datePickerMode = UIDatePickerMode.DateAndTime
+      
+      // 일주일 전 ~ 일주일 후
+      let week : NSTimeInterval = 60 * 60 * 24 * 7
+      datePicker.minimumDate = NSDate(timeIntervalSinceNow: -week)
+      datePicker.maximumDate = NSDate(timeIntervalSinceNow: week)
+      
+      // 뷰 구조에 추가
+      self.view.addSubview(datePicker)
+      
+      // 날짜 변경 이벤트
+      datePicker.addTarget(self, action: Selector("timeChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+   }
+   
+   func timeChanged(sender : AnyObject) {
+      let datePicker = sender as! UIDatePicker
+      let date = datePicker.date
+      
+      // 데이트 포매터
+      let dateFormatter = NSDateFormatter()
+      dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
+      dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+      
+      // 날짜 형식에 맞는 문자열 얻기
+      let dateStr = dateFormatter.stringFromDate(date)
+      print("선택한 날짜 : \(dateStr)")
+   }
+   
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+   
+   
 }
 
