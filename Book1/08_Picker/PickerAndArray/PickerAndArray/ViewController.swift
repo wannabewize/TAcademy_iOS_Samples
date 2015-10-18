9d5e99b0dcd582557c2bd12cbec507fe7a92417d
Lee Jaehoon <wannabewize@gmail.com>
2015-10-18 17:21:44 +0900

    피커뷰의 델리게이트 중 옵셔널 변경사항 반영

--- Book1/08_Picker/PickerAndArray/PickerAndArray/ViewController.swift
+++ Book1/08_Picker/PickerAndArray/PickerAndArray/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
   
-  var data : [String]!
+  let data = ["MacBook Air", "MacBook pro", "MacBook Pro Retina", "Mac Mini", "iMac", "Mac Pro", "iPhone", "iPod Touch"]
   
   func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
     return 1
@@ -20,24 +20,12 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     return data.count
   }
   
-  func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+  func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
     return data[row]
-  }
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    data = ["MacBook Air", "MacBook pro", "MacBook Pro Retina", "Mac Mini", "iMac", "Mac Pro", "iPhone", "iPod Touch"]
   }
   
   func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
     print("Select : " + data[row])
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
