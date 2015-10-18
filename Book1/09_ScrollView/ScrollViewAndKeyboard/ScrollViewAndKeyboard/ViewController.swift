d3e655c28d94cf9f895c0dcb9061ee84cbba5c31
Lee Jaehoon <wannabewize@gmail.com>
2015-10-18 17:59:28 +0900

    스크롤 뷰와 키보드, 스토리보드 재작성

--- Book1/09_ScrollView/ScrollViewAndKeyboard/ScrollViewAndKeyboard/ViewController.swift
+++ Book1/09_ScrollView/ScrollViewAndKeyboard/ScrollViewAndKeyboard/ViewController.swift
@@ -68,17 +68,5 @@ class ViewController: UIViewController {
     // 없으면 nil 반환
     return nil
   }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  
 }
 
