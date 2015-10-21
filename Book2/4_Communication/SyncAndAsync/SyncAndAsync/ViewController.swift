a2968956f3ae9cff65c99bc4bf1b79b0eba0c7b1
wannabewize <wannabewize@gmail.com>
2015-10-22 02:43:40 +0900

    Xcode7, iOS9

--- Book2/4_Communication/SyncAndAsync/SyncAndAsync/ViewController.swift
+++ Book2/4_Communication/SyncAndAsync/SyncAndAsync/ViewController.swift
@@ -9,67 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-  // 이미지 뷰의 아웃렛
-  @IBOutlet weak var imageView: UIImageView!
-  let urlStr = "http://upload.wikimedia.org/wikipedia/commons/4/4d/Klimt_-_Der_Kuss.jpeg"
-
-  @IBOutlet weak var indicator: UIActivityIndicatorView!
-  
-  @IBAction func handleSync(sender: AnyObject) {
-    imageView.image = nil
-
-    // 인디케이터 시작 - 그러나 동작하지 않는다.
-    indicator.startAnimating()
-
-    let url = NSURL(string: urlStr)!
-    
-    //NSData(contentsOfURL: url)!
-    var error : NSError?
-    let data = NSData(contentsOfURL: url, options: NSDataReadingOptions.DataReadingUncached, error: &error)!
-    // 이미지 생성. 이미지 뷰에 반영
-    let image = UIImage(data: data)
-    imageView.image = image
-    
-    indicator.stopAnimating()
-  }
-  
-  
-  @IBAction func handleASync(sender: AnyObject) {
-    imageView.image = nil
-    
-    // 인디케이터 시작
-    indicator.startAnimating()
-    
-    let url = NSURL(string: urlStr)!
-    let request = NSURLRequest(URL: url)
-
-    // 이미지를 화면에 반영하려면 메인 쓰레드에서
-    let queue = NSOperationQueue.mainQueue()
-    NSURLConnection.sendAsynchronousRequest(request, queue: queue) { (response : NSURLResponse!, data : NSData!, error : NSError!) -> Void in
+   // 이미지 뷰의 아웃렛
+   @IBOutlet weak var imageView: UIImageView!
+   let urlStr = "http://upload.wikimedia.org/wikipedia/commons/4/4d/Klimt_-_Der_Kuss.jpeg"
+   
+   @IBOutlet weak var indicator: UIActivityIndicatorView!
+   
+   @IBAction func handleSync(sender: AnyObject) {
+      imageView.image = nil
+      
+      // 인디케이터 시작 - 그러나 동작하지 않는다.
+      indicator.startAnimating()
+      
+      let url = NSURL(string: urlStr)!
+      
+      let data = NSData(contentsOfURL: url)!
       let image = UIImage(data: data)
-      self.imageView.image = image
-      println("is Main thread? \(NSThread.isMainThread())")
-      // 인디케이터 중지
-      self.indicator.stopAnimating()
-    }
-  }
-  
-  @IBAction func handleTask(sender: AnyObject) {
-    let url = NSURL(string: urlStr)!
-    let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-    
-    var error : NSError?
-    let task = session.dataTaskWithURL(url, completionHandler: { (data : NSData!, response : NSURLResponse!, error : NSError!) -> Void in
-      println("isMainThread ? \(NSThread.isMainThread()) data size : \(data.length)")
-      NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
-        let image = UIImage(data: data)
-        self.imageView.image = image
-        self.indicator.stopAnimating()
+      imageView.image = image
+      
+      indicator.stopAnimating()
+   }
+   
+   
+   @IBAction func handleASync(sender: AnyObject) {
+      imageView.image = nil
+      
+      // 인디케이터 시작
+      indicator.startAnimating()
+      
+      let url = NSURL(string: urlStr)!
+      let request = NSMutableURLRequest(URL: url)
+      
+      // 이미지를 화면에 반영하려면 메인 쓰레드에서
+      let queue = NSOperationQueue.mainQueue()
+      NSURLConnection.sendAsynchronousRequest(request, queue: queue) { (response : NSURLResponse?, data : NSData?, error : NSError?) -> Void in
+         if nil != error {
+            print("Error : \(error)")
+            return
+         }
+         let image = UIImage(data: data!)
+         self.imageView.image = image
+         print("is Main thread? \(NSThread.isMainThread())")
+         // 인디케이터 중지
+         self.indicator.stopAnimating()
+      }
+   }
+   
+   @IBAction func handleTask(sender: AnyObject) {
+      let url = NSURL(string: urlStr)!
+      let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+      
+      let task = session.dataTaskWithURL(url, completionHandler: { (data : NSData?, response : NSURLResponse?, error : NSError?) -> Void in
+         if nil != error {
+            print("Error : \(error)")
+            return
+         }
+         
+         print("isMainThread ? \(NSThread.isMainThread()) data size : \(data!.length)")
+         NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+            let image = UIImage(data: data!)
+            self.imageView.image = image
+            self.indicator.stopAnimating()
+         })
       })
-    })
-    
-    self.indicator.startAnimating()
-    task.resume()
-  }
+      
+      self.indicator.startAnimating()
+      task.resume()
+   }
 }
 
--- Book2/4_Communication/WebView/WebView/ViewController.swift
+++ Book2/4_Communication/WebView/WebView/ViewController.swift
@@ -9,26 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController, UIWebViewDelegate {
-  @IBOutlet weak var textField: UITextField!
-  @IBOutlet weak var webView: UIWebView!
-
-  // 엔터로 웹뷰 로딩
-  @IBAction func didEndOnExit(sender: AnyObject) {
-    // http를 입력해야 한다.
-    let urlStr = textField.text
-    if let url = NSURL(string: urlStr) {
-      let request = NSURLRequest(URL: url)
-      webView.loadRequest(request)
-    }
-  }
-  
-  func webViewDidFinishLoad(webView: UIWebView) {
-    println("로딩 완료!")
-  }
-  
-  func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
-    // 에러 처리
-    println("에러!")
-  }
+   @IBOutlet weak var textField: UITextField!
+   @IBOutlet weak var webView: UIWebView!
+   
+   // 엔터로 웹뷰 로딩
+   @IBAction func didEndOnExit(sender: AnyObject) {
+      // http를 입력해야 한다.
+      let urlStr = textField.text!
+      loadWebview(urlStr)
+   }
+   
+   func loadWebview(url : String) {
+      if let url = NSURL(string: url) {
+         let request = NSURLRequest(URL: url)
+         webView.loadRequest(request)
+      }
+   }
+   
+   override func viewWillAppear(animated: Bool) {
+      loadWebview("http://apple.com")
+   }
+   
+   func webViewDidFinishLoad(webView: UIWebView) {
+      print("로딩 완료!")
+   }
+   
+   func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
+      // 에러 처리
+      print("에러!")
+   }
 }
 
