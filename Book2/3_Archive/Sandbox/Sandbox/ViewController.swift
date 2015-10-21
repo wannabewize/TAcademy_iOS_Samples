c81c4404ce6537a7d57ce8268cdc97d69de423b6
Lee Jaehoon <wannabewize@gmail.com>
2015-10-21 21:29:35 +0900

    Xcode7, iOS9 적용

--- Book2/3_Archive/Sandbox/Sandbox/ViewController.swift
+++ Book2/3_Archive/Sandbox/Sandbox/ViewController.swift
@@ -9,69 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // 번들 폴더 경로
     let bundlePath = NSBundle.mainBundle().bundlePath
-    println("bundle path : \(bundlePath)")
+    print("bundle path : \(bundlePath)")
     // 번들 내 리소스 경로 얻기
     let imagePath = NSBundle.mainBundle().pathForResource("image", ofType: "png")!
-    println("image path : \(imagePath)")
+    print("image path : \(imagePath)")
     
     // 홈 폴더
     let homeFolder = NSHomeDirectory()
-    println("home : \(homeFolder)")
+    print("home : \(homeFolder)")
     
     // 도큐먼트 폴더
-    let docFolders = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true) as! [String]
-    let docFolder = docFolders[0]
-    println("doc folder : \(docFolder)")
+    let docFolders = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+    let docFolder = docFolders[0] as NSString
+    print("doc folder : \(docFolder)")
     
     // 파일 경로
-    let filePath = docFolder.stringByAppendingPathComponent("image.png")
-    println("file path : \(filePath)")
+    let filePath = "file"//docFolder.stringByAppendingPathComponent("image.png")
+    print("file path : \(filePath)")
     
     let fm = NSFileManager.defaultManager()
     if false == fm.fileExistsAtPath(filePath) {
-      println("파일 없음 - 복사")
-      var error : NSError?
-      fm.copyItemAtPath(imagePath, toPath: filePath, error: &error)
+      print("파일 없음 - 복사")
+      
+      do {
+        try fm.copyItemAtPath(imagePath, toPath: filePath)
+      }
+      catch let err {
+        print("파일 복사 오류 \(err)")
+      }
     }
     else {
-      println("파일 있음")
+      
     }
     
-//    let tmpPath = NSTemporaryDirectory()
-//    println("tmp path : \(tmpPath)")
-//    var error : NSError?
-//    let str = "Hello World"
-//    str.writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding, error: &error)
-//
-//    let str2 = String(contentsOfFile: filePath, encoding: NSUTF8StringEncoding, error: &error)
-    
+    let tmpPath = NSTemporaryDirectory()
+    print("tmp path : \(tmpPath)")
+    let str = "Hello World"
+    do {
+      try str.writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding)
+    }
+    catch {
+      print("문자열 파일 저장 에러")
+    }
 
-    // Do any additional setup after loading the view, typically from a nib.
-
-    let filePath3 = docFolder.stringByAppendingPathComponent("array.plist")
-    let array : NSArray = [1,2,3]
-    array.writeToFile(filePath3, atomically: true)
+    do {
+      let str2 : String;
+//      try str2 = String(contentsOfFile: filePath)
+      try str2 = String(contentsOfFile: filePath, encoding: NSUTF8StringEncoding)
+      print("파일 읽기 \(str2)")
+      
+    }
+    catch {
+      print("문자열 저장 파일 읽기 오류")
+    }
     
-    let array2 = NSArray(contentsOfFile: filePath3)
-    println("공유일 : \(array2)")
+    // 배열 - NSArray
+    let array : NSArray = ["신정", "구정", "삼일절", "노동절", "어린이날"]
+    // 도큐먼트 폴더에 파일 경로
+    let arrayFilePath = docFolder.stringByAppendingPathComponent("holiday.plist")
+    // 파일로 저장
+    array.writeToFile(arrayFilePath, atomically: true)
+    
+    
+    let array2 = NSArray(contentsOfFile: arrayFilePath)
+    print("공유일 : \(array2)")
     
     
     let filePath4 = NSBundle.mainBundle().pathForResource("holidays", ofType: "plist")!
     let array3 = NSArray(contentsOfFile: filePath4)
-    println("plist : \(array3)")
-
+    print("plist : \(array3)")
+    
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  
 }
 
