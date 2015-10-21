53da115631f871942a469ec05340ea8b65f5dcff
wannabewize <wannabewize@gmail.com>
2015-10-22 03:14:23 +0900

    Xcode7, iOS9 적용

--- Book2/4_Communication/TopSongWithJSON/TopSongWithJSON/ViewController.swift
+++ Book2/4_Communication/TopSongWithJSON/TopSongWithJSON/ViewController.swift
@@ -9,72 +9,77 @@
 import UIKit
 
 struct SongInfo {
-  var title : String?
-  var image : String?
-  var artist : String?
-  init() {}
+   var title : String?
+   var image : String?
+   var artist : String?
+   init() {}
 }
 
 class ViewController: UIViewController, UITableViewDataSource {
-  let urlStr = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=25/json"
-  
-  var songList = [SongInfo]()
-  @IBOutlet weak var tableView: UITableView!
-  
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return songList.count
-  }
-  
-  // 이미지 캐쉬
-  var imageCache = [NSURL : UIImage]()
-  var operationQueue = NSOperationQueue()
-  
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    var cell = tableView.dequeueReusableCellWithIdentifier("SongCell", forIndexPath: indexPath) as! UITableViewCell
-    let one = songList[indexPath.row]
-    cell.textLabel!.text = one.title
-    println("\(indexPath.row) image \(one.image)")
-    // 이미지 경로가 있으면
-    if let urlStr = one.image {
-      if let url = NSURL(string: urlStr) {
-        let data = NSData(contentsOfURL: url)!
-        let image = UIImage(data: data)!
-        cell.imageView?.image = image
+   let urlStr = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=25/json"
+   
+   var songList = [SongInfo]()
+   @IBOutlet weak var tableView: UITableView!
+   
+   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return songList.count
+   }
+   
+   // 이미지 캐쉬
+   var imageCache = [NSURL : UIImage]()
+   var operationQueue = NSOperationQueue()
+   
+   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCellWithIdentifier("SongCell", forIndexPath: indexPath)
+      let one = songList[indexPath.row]
+      cell.textLabel!.text = one.title
+      print("\(indexPath.row) image \(one.image)")
+      // 이미지 경로가 있으면
+      if let urlStr = one.image {
+         if let url = NSURL(string: urlStr) {
+            let data = NSData(contentsOfURL: url)!
+            let image = UIImage(data: data)!
+            cell.imageView?.image = image
+         }
       }
-    }
-    return cell
-  }
-  
-  func parseJSON() {
-    let url = NSURL(string: urlStr)!
-    let data = NSData(contentsOfURL: url)!
-    var error : NSError?
-    
-    // JSON 파싱
-    let result = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: &error) as! NSDictionary
-    // JSON 구조를 따라서 정보 찾기
-    if let feed = result["feed"] as? NSDictionary {
-      if let entry = feed["entry"] as? NSArray {
-        for one in entry {
-          var song = SongInfo()
-          
-          let title = (one["title"] as! NSDictionary)["label"] as! String
-          song.title = title
-          
-          let images = one["im:image"] as! NSArray
-          if let image = (images[0] as! NSDictionary)["label"] as? String {
-            song.image = image
-          }
-          
-          songList.append(song)
-        }
-        tableView.reloadData()
+      return cell
+   }
+   
+   func parseJSON() {
+      let url = NSURL(string: urlStr)!
+      let data = NSData(contentsOfURL: url)!
+      
+      do {
+         // JSON 파싱
+         let result = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0)) as! NSDictionary
+
+         // JSON 구조를 따라서 정보 찾기
+         if let feed = result["feed"] as? NSDictionary {
+            if let entry = feed["entry"] as? NSArray {
+               for one in entry {
+                  var song = SongInfo()
+                  
+                  let title = (one["title"] as! NSDictionary)["label"] as! String
+                  song.title = title
+                  
+                  let images = one["im:image"] as! NSArray
+                  if let image = (images[0] as! NSDictionary)["label"] as? String {
+                     song.image = image
+                  }
+                  
+                  songList.append(song)
+               }
+               tableView.reloadData()
+            }
+         }
       }
-    }
-  }
-  
-  override func viewWillAppear(animated: Bool) {
-    parseJSON()
-  }
+      catch {
+         
+      }
+   }
+   
+   override func viewWillAppear(animated: Bool) {
+      parseJSON()
+   }
 }
 
