35a7c036bea5717981ec660b00ec7982cd15d4a8
Lee Jaehoon <wannabewize@gmail.com>
2015-10-24 19:43:59 +0900

    안쓰는 프로젝트 삭제

--- Book2/4_Communication/CodeSnippet/CodeSnippet/MyPlayground.playground/section-1.swift
+++
@@ -1,9 +0,0 @@
-// Playground - noun: a place where people can play
-
-import UIKit
-
-var str = "Hello, playground"
-
-
-let urlStr = "https://www.google.com/search?q=아이폰"
-let encoded = urlStr.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
