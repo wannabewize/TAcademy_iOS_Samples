f867d006003a1a702fd066d52ece586809bf1861
Lee Jaehoon <wannabewize@gmail.com>
2016-03-14 13:48:00 +0900

    Image Example

---
+++ Book1/03_SceneAndContent/Images/Images/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Images
+//
+//  Created by wannabewize on 2016. 3. 11..
+//  Copyright © 2016년 VanillaStep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
