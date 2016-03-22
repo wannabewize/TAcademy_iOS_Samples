b3c777e5a628274051c5bf053ac981c9d6644751
wannabewize <wannabewize@gmail.com>
2016-03-23 00:20:09 +0900

    if-let 멀티 바인딩 코드로 변경

--- Book2/4_Communication/TopSongWithJSON/TopSongWithJSON/ViewController.swift
+++ Book2/4_Communication/TopSongWithJSON/TopSongWithJSON/ViewController.swift
@@ -34,47 +34,42 @@ class ViewController: UIViewController, UITableViewDataSource {
       let one = songList[indexPath.row]
       cell.textLabel!.text = one.title
       print("\(indexPath.row) image \(one.image)")
-      // 이미지 경로가 있으면
-      if let urlStr = one.image {
-         if let url = NSURL(string: urlStr) {
-            let data = NSData(contentsOfURL: url)!
-            let image = UIImage(data: data)!
+      // 이미지 경로가 있으면 - 동기식 방식 - 비동기식으로 변환 필요
+      if let urlStr = one.image,
+         let url = NSURL(string: urlStr),
+         let data = NSData(contentsOfURL: url),
+         let image = UIImage(data: data) {
             cell.imageView?.image = image
-         }
       }
       return cell
    }
    
    func parseJSON() {
-      let url = NSURL(string: urlStr)!
-      let data = NSData(contentsOfURL: url)!
-      
-      do {
-         // JSON 파싱
-         let result = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0)) as! NSDictionary
-
-         // JSON 구조를 따라서 정보 찾기
-         if let feed = result["feed"] as? NSDictionary {
-            if let entry = feed["entry"] as? NSArray {
-               for one in entry {
-                  var song = SongInfo()
-                  
-                  let title = (one["title"] as! NSDictionary)["label"] as! String
-                  song.title = title
-                  
-                  let images = one["im:image"] as! NSArray
-                  if let image = (images[0] as! NSDictionary)["label"] as? String {
-                     song.image = image
+      if let url = NSURL(string: urlStr), let data = NSData(contentsOfURL: url) {
+         do {
+            // JSON 파싱
+            if let result = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0)) as? NSDictionary,
+               let feed = result["feed"] as? NSDictionary,
+               let entry = feed["entry"] as? NSArray {
+                  for one in entry {
+                     var song = SongInfo()
+                     
+                     let title = (one["title"] as! NSDictionary)["label"] as! String
+                     song.title = title
+                     
+                     let images = one["im:image"] as! NSArray
+                     if let image = (images[0] as! NSDictionary)["label"] as? String {
+                        song.image = image
+                     }
+                     
+                     songList.append(song)
                   }
-                  
-                  songList.append(song)
-               }
-               tableView.reloadData()
+                  tableView.reloadData()
             }
          }
-      }
-      catch {
-         
+         catch let error {
+            print("Error : ", error)
+         }
       }
    }
    
