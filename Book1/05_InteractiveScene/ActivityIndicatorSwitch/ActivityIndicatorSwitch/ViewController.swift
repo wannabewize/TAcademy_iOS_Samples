bcbafadef14a07a4f0c819a12260752b989ecb11
wannabewize <wannabewize@gmail.com>
2016-06-02 01:39:21 +0900

    Xcode 7.3, Swift 2.2 적용

--- Book1/05_InteractiveScene/ActivityIndicatorSwitch/ActivityIndicatorSwitch/ViewController.swift
+++ Book1/05_InteractiveScene/ActivityIndicatorSwitch/ActivityIndicatorSwitch/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // 액티비티 인디케이터의 아웃렛
-    @IBOutlet weak var indicator: UIActivityIndicatorView!
-    // 스위치의 ValueChanged의 액션 메소드
-    @IBAction func switchChanged(sender: AnyObject) {
-        let onOffSwitch = sender as! UISwitch
-        if onOffSwitch.on {
-            indicator.startAnimating()
-        }
-        else {
-            indicator.stopAnimating()
-        }
-    }
+   // 액티비티 인디케이터의 아웃렛
+   @IBOutlet weak var indicator: UIActivityIndicatorView!
+   // 스위치의 ValueChanged의 액션 메소드
+   @IBAction func switchChanged(sender: AnyObject) {
+      let onOffSwitch = sender as! UISwitch
+      if onOffSwitch.on {
+         indicator.startAnimating()
+      }
+      else {
+         indicator.stopAnimating()
+      }
+   }
 }
 
--- Book1/05_InteractiveScene/SegmentedControl/SegmentedControl/ViewController.swift
+++ Book1/05_InteractiveScene/SegmentedControl/SegmentedControl/ViewController.swift
@@ -4,35 +4,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    // png는 확장자 생략 가능, 옵셔널 체크 필요
-    let rain = UIImage(named: "rain")!
-//    let rain2 = rain.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
-    let thunder = UIImage(named: "thunder")!
-    let sun = UIImage(named: "sun")!
-
-    // 배열
-    let items = [rain, thunder, sun]
-    
-    let control = UISegmentedControl(items: items)
-    control.frame = CGRectMake(20, 200, 300, 40)
-    self.view.addSubview(control)
-    
-    control.addTarget(self, action: Selector("handleSegmentChange:"), forControlEvents: UIControlEvents.ValueChanged)
-  }
-  
-  func handleSegmentChange(sender : AnyObject) {
-    let control = sender as! UISegmentedControl
-    switch control.selectedSegmentIndex {
-    case 0:
-      print("소나기")
-    case 1:
-      print("천둥,번개")
-    default:
-      print("맑음")
-    }
-  }
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      // png는 확장자 생략 가능, 옵셔널 체크 필요
+      let rain = UIImage(named: "rain")!
+      //    let rain2 = rain.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+      let thunder = UIImage(named: "thunder")!
+      let sun = UIImage(named: "sun")!
+      
+      // 배열
+      let items = [rain, thunder, sun]
+      
+      let control = UISegmentedControl(items: items)
+      control.frame = CGRectMake(20, 200, 300, 40)
+      self.view.addSubview(control)
+      
+      control.addTarget(self, action: #selector(handleSegmentChange(_:)), forControlEvents: UIControlEvents.ValueChanged)
+   }
+   
+   func handleSegmentChange(sender : AnyObject) {
+      let control = sender as! UISegmentedControl
+      switch control.selectedSegmentIndex {
+      case 0:
+         print("소나기")
+      case 1:
+         print("천둥,번개")
+      default:
+         print("맑음")
+      }
+   }
 }
 
